b0f25a6867f69868dae48255d41fc86de39f3fde
KateC08 <coming.katelyn08@gmail.com>
2024-10-03 22:15:48 +0800

    Added a Class named Person and a methods to display the ouput

--- Methods.js
+++ Methods.js
@@ -10,3 +10,28 @@ const person = {
    console.log("Age: "  +person.age);
    console.log("Gender: " + person.gender);  
    console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
+
+   //Class named Person
+class Person {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    //Solution 1: Display via console.log
+    displayInfo() { //Process and Output
+       console.log("Name: " + this.name);
+       console.log("Age: "  + this.age);
+       console.log("Gender: " + this.gender);
+    }
+    //Solution 2: Display via HTML
+    displayViaHTML() {
+        const personElement = document.getElementById('person1');
+        personElement.innerHTML = `
+            <strong>Name:</strong> ${this.name}<br>
+            <strong>Age:</strong> ${this.age}<br>
+            <strong>Gender:</strong> ${this.gender}
+        `;
+    }
+}
