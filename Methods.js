c79ecc866ca17cd2ea89ae5ccfecece2627b2ea4
KateC08 <coming.katelyn08@gmail.com>
2024-10-03 22:14:59 +0800

    Added an objects and properties and its values, and calls it via console.log

---
+++ Methods.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Kate",
+    age: 19,
+    gender: "Female"
+   }
+
+   //Process and Output
+   console.log("Hello There!");
+   console.log("Name: " + person.name);
+   console.log("Age: "  +person.age);
+   console.log("Gender: " + person.gender);  
+   console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
