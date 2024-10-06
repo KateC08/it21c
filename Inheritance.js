3661f2434426a0ea40eee85d804a1c6e8016e88d
KateC08 <coming.katelyn08@gmail.com>
2024-10-06 16:45:28 +0800

    Added a constructor of the name, age and gender

--- Inheritance.js
+++ Inheritance.js
@@ -3,4 +3,10 @@ class Person {
     #name;
     #age;
     #gender;
+
+    constructor(name, age, gender) {
+        this.#name = name;
+        this.#age = age;
+        this.#gender = gender;
+    }
 }
